b199cdbf7eead965fd36dd4e56663c5eb36e488a
Feng Biao <smilemovethecrowd@163.com>
2021-09-22 19:28:53 +0800

    初始化账户

--- trade_app.py
+++ trade_app.py
@@ -63,36 +63,40 @@ class hb_trade(object):
 
 if __name__ == '__main__':
     #自己的火币账户的access_key, secret_key (火币每个主账号能创建200个子账号，尽量使用子账号操作,防范风险)
-    access_key = 'XXXXXXXXXXXXXXXXXXXX'
-    secret_key = 'XXXXXXXXXXXXXXXXXXXXXXX'
+    access_key = '4afd8340-5ad0623d-bgbfh5tv3f-55129'
+    secret_key = '2b67b92b-fd41e23e-5f25504a-9cf3a'
     huobi_trade = hb_trade(access_key, secret_key)              #初始化交易类
 
-    usdt_balance = huobi_trade.trade.get_balance('usdt')        #查询稳定币usdt的余额
+    # usdt_balance = huobi_trade.trade.get_balance('usdt')        #查询稳定币usdt的余额
+    cny = huobi_trade.trade.get_amount_valuation('CNY')
 
-    coin_code = 'btc.usdt'                                      #定义交易对 
-    init_money = 10.00                                          #买入金额(单位:usdt)
-    buy_json = huobi_trade.order_value(coin_code, init_money)   #用1000USDT 买入btc
+    print('当前账户余额为%s元:' + str(cny))
+   
+    #coin_code = 'btc.usdt'                                      #定义交易对 
+    #init_money = 10.00                                            #买入金额(单位:usdt)
+    #buy_json = huobi_trade.order_value(coin_code, init_money)   #用10.00USDT 买入btc
+   
     #  buy_json 返回字典类型，买入成交回报：
     # {'单号':'2722295','成交数量':0.000177,'成交金额':'10.0000','扣手续费':3.562403,'平均价格':56497.18}
 
-    amount = huobi_trade.trade.get_amount(coin_code)            #查询btc.usdt交易对的数量,有精度控制
-    print('当前账户%s数量:' % (coin_code) + str(amount))
+    #amount = huobi_trade.trade.get_amount(coin_code)            #查询btc.usdt交易对的数量,有精度控制
+    #print('当前账户%s数量:' % (coin_code) + str(amount))
 
-    sell_json = huobi_trade.order_target(coin_code, amount)     #卖出当前持仓所有btc
+    #sell_json = huobi_trade.order_target(coin_code, amount)     #卖出当前持仓所有btc
     # sell_json 返回字典类型，卖出成交回报：
     # {'单号':'2722297','成交数量': 0.000177,'成交金额': 9.9327,'扣手续费':0.019865,'平均价格': 56229.7}
 
     #查询当前未成交订单 入参是定义的交易对
     #详细返回参数请参考 https://huobiapi.github.io/docs/spot/v1/cn/#95f2078356
-    open_order = huobi_trade.trade.check_open_order(coin_code)
+    #open_order = huobi_trade.trade.check_open_order(coin_code)
 
     #查询订单详情 入参是单号
     #详细返回参数请参考 https://huobiapi.github.io/docs/spot/v1/cn/#92d59b6aad
-    find_order = huobi_trade.trade.find_order('272249503181077')
+    #find_order = huobi_trade.trade.find_order('272249503181077')
 
     #获取成交明细 入参是单号
     #详细返回参数请参考 https://huobiapi.github.io/docs/spot/v1/cn/#56c6c47284
-    order_details = huobi_trade.trade.get_order_details('272249503181077')
+    #order_details = huobi_trade.trade.get_order_details('272249503181077')
 
     #根据成交单号获取真实扣费情况 入参是单号
-    real_fees = huobi_trade.trade.get_real_fees('272249503181077')
+    #real_fees = huobi_trade.trade.get_real_fees('272249503181077')
